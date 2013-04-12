3e938e5beac54137fba46b306db29c885f255e86
aluno <aluno@aluno-desktop>
2013-04-12 10:37:39 -0300

    commit 12/04

--- Trabalho01/src/br/edu/ifnmg/tads/Trabalho01/Classes/Pessoa.java
+++ Trabalho01/src/br/edu/ifnmg/tads/Trabalho01/Classes/Pessoa.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.edu.ifnmg.tads.Trbalho01.Classes;
+package br.edu.ifnmg.tads.Trabalho01.Classes;
 
 import java.util.List;
 import java.util.Objects;
