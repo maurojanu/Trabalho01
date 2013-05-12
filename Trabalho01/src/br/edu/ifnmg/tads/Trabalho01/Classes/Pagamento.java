8921fb80203aa3c51c9fe5b30cc109e0b039b23f
Mauro <Mauro@MuroSergio>
2013-05-12 13:14:08 -0300

    alteração classe pagamentos

--- Trabalho01/src/br/edu/ifnmg/tads/Trabalho01/Classes/Pagamento.java
+++ Trabalho01/src/br/edu/ifnmg/tads/Trabalho01/Classes/Pagamento.java
@@ -12,21 +12,15 @@ import java.util.Objects;
  */
 public class Pagamento {
     private int id;
-    private String Tipo;
     private String Descricao;
 
-    public Pagamento(int id, String Tipo, String Descricao) {
+    public Pagamento(int id, String Descricao) {
         this.id = id;
-        this.Tipo = Tipo;
         this.Descricao = Descricao;
     }
 
     public int getId() {
         return id;
-    }
-
-    public String getTipo() {
-        return Tipo;
     }
 
     public String getDescricao() {
@@ -37,20 +31,15 @@ public class Pagamento {
         this.id = id;
     }
 
-    public void setTipo(String Tipo) {
-        this.Tipo = Tipo;
-    }
-
     public void setDescricao(String Descricao) {
         this.Descricao = Descricao;
     }
 
     @Override
     public int hashCode() {
-        int hash = 7;
-        hash = 89 * hash + this.id;
-        hash = 89 * hash + Objects.hashCode(this.Tipo);
-        hash = 89 * hash + Objects.hashCode(this.Descricao);
+        int hash = 5;
+        hash = 29 * hash + this.id;
+        hash = 29 * hash + Objects.hashCode(this.Descricao);
         return hash;
     }
 
@@ -66,9 +55,6 @@ public class Pagamento {
         if (this.id != other.id) {
             return false;
         }
-        if (!Objects.equals(this.Tipo, other.Tipo)) {
-            return false;
-        }
         if (!Objects.equals(this.Descricao, other.Descricao)) {
             return false;
         }
@@ -77,8 +63,10 @@ public class Pagamento {
 
     @Override
     public String toString() {
-        return "Pagamento{" + "id=" + id + ", Tipo=" + Tipo + ", Descricao=" + Descricao + '}';
+        return "Pagamento{" + "id=" + id + ", Descricao=" + Descricao + '}';
     }
+
+   
     
     
 }
