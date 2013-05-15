87944d4562a9ac8d8a974628f65249d66d6cb354
Mauro <Mauro@MuroSergio>
2013-05-15 16:31:50 -0300

    listando usuarios

--- Trabalho01/src/br/edu/ifnmg/tads/Trabalho01/Classes/InterfaceUsuario/frmListagemUsuario.java
+++ Trabalho01/src/br/edu/ifnmg/tads/Trabalho01/Classes/InterfaceUsuario/frmListagemUsuario.java
@@ -4,19 +4,49 @@
  */
 package br.edu.ifnmg.tads.Trabalho01.Classes.InterfaceUsuario;
 
+import br.edu.ifnmg.tads.Trabalho01.DataAcess.DAOUsuario;
+import br.edu.ifnmg.tads.Trabalho01.Classes.Usuario;
+
+import java.util.List;
+import java.util.Vector;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableModel;
 /**
  *
  * @author Mauro
  */
 public class frmListagemUsuario extends javax.swing.JInternalFrame {
 
+    DAOUsuario dao;
     /**
      * Creates new form frmListagemUsuario
      */
     public frmListagemUsuario() {
         initComponents();
+         dao = new DAOUsuario();
+        List<Usuario> usuarios = dao.listarTodos();
+        
+        preencheTabela(usuarios);
     }
-
+private void preencheTabela(List<Usuario> lista) {
+        DefaultTableModel model = new DefaultTableModel();
+        model.addColumn("Id");
+        model.addColumn("Nome");
+        
+        
+        for (Usuario u : lista) {
+            Vector valores = new Vector();
+            valores.add(0,u.getId());
+            valores.add(1,u.getNome());
+            //valores.add(2,u.getSenha());
+            
+            model.addRow(valores);
+        }
+        tblListar.setModel(model);
+        tblListar.repaint();
+        
+    }
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -26,25 +56,95 @@ public class frmListagemUsuario extends javax.swing.JInternalFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        btnListar = new javax.swing.JButton();
+        txtFiltro = new javax.swing.JTextField();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tblListar = new javax.swing.JTable();
+
         setClosable(true);
         setIconifiable(true);
         setMaximizable(true);
         setResizable(true);
         setTitle("Listando Usuários");
 
+        btnListar.setText("Filtrar");
+        btnListar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnListarActionPerformed(evt);
+            }
+        });
+
+        tblListar.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {},
+                {},
+                {},
+                {}
+            },
+            new String [] {
+
+            }
+        ));
+        tblListar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                tblListarMouseClicked(evt);
+            }
+        });
+        jScrollPane1.setViewportView(tblListar);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 394, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(37, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(txtFiltro, javax.swing.GroupLayout.PREFERRED_SIZE, 288, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(btnListar)))
+                .addGap(49, 49, 49))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 278, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(25, 25, 25)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnListar)
+                    .addComponent(txtFiltro, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(27, 27, 27)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 184, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(42, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnListarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnListarActionPerformed
+        // TODO add your handling code here:
+        Usuario u = new Usuario();
+        u.setNome(txtFiltro.getText());
+        
+        List<Usuario> lista = dao.buscar(u);
+        
+        preencheTabela(lista);
+    }//GEN-LAST:event_btnListarActionPerformed
+
+    private void tblListarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tblListarMouseClicked
+        // TODO add your handling code here:
+        /*Object valor = tblListar.getValueAt( tblListar.getSelectedRow(), 0);
+        Usuario u = dao.Abrir((int)valor);
+        frmEditarUsuario janela = new frmEditarUsuario(u, dao);
+        this.getParent().add(janela);
+        janela.setVisible(true);
+        this.setVisible(false);*/
+    }//GEN-LAST:event_tblListarMouseClicked
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnListar;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable tblListar;
+    private javax.swing.JTextField txtFiltro;
     // End of variables declaration//GEN-END:variables
 }
--- Trabalho01/src/br/edu/ifnmg/tads/Trabalho01/DataAcess/DAOUsuario.java
+++ Trabalho01/src/br/edu/ifnmg/tads/Trabalho01/DataAcess/DAOUsuario.java
@@ -6,7 +6,11 @@ package br.edu.ifnmg.tads.Trabalho01.DataAcess;
 
 import br.edu.ifnmg.tads.Trabalho01.Classes.Usuario;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -44,4 +48,103 @@ public class DAOUsuario {
             return false;
         }
      }
+     public List<Usuario> listarTodos() {
+        try {
+            PreparedStatement comando = bd.getConexao().prepareStatement("select * from usuario ");
+            ResultSet resultado = comando.executeQuery();
+            // Cria uma lista de produtos vazia
+            List<Usuario> usuarios = new LinkedList<>();
+            while (resultado.next()) {
+                // Inicializa um objeto de produto vazio
+                Usuario tmp = new Usuario();
+                // Pega os valores do retorno da consulta e coloca no objeto
+                tmp.setId(resultado.getInt("Id"));
+                tmp.setNome(resultado.getString("Nome"));
+                
+                // Pega o objeto e coloca na lista
+                usuarios.add(tmp);
+            }
+            return usuarios;
+        } catch (SQLException ex) {
+            Logger.getLogger(DAOUsuario.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+   
+    public Usuario Abrir(int id) {
+        try {
+            Usuario usuario = new Usuario();
+
+            PreparedStatement comando = bd.getConexao().prepareStatement("select * from usuario where Id = ?");
+            comando.setInt(1, id);
+            ResultSet resultado = comando.executeQuery();
+
+            resultado.first();
+
+            usuario.setId(resultado.getInt("Id"));
+            usuario.setNome(resultado.getString("Nome"));
+            
+
+            return usuario;
+
+        } catch (SQLException ex) {
+            Logger.getLogger(DAOUsuario.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+
+    public boolean Apagar(Usuario obj) {
+        try {
+            PreparedStatement comando = bd.getConexao().prepareStatement("delete from usuario where Id = ?");
+            comando.setInt(0, obj.getId());
+            comando.executeUpdate();
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(DAOUsuario.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+   public List<Usuario> buscar(Usuario filtro) {
+        try {
+            
+            String sql = "select * from usuario ";
+            String where = "";
+            
+            if(filtro.getNome().length() > 0){
+                where = "Nome like '%"+filtro.getNome()+"%'";
+            }
+            
+           
+            
+            if (filtro.getId() > 0) {
+                if(where.length() > 0) 
+                    where = where + " and ";
+                where = where + " Id = " + filtro.getId();
+            }
+            
+            if(where.length() > 0){
+                sql = sql + " where " + where;
+            }
+            
+            Statement comando = bd.getConexao().createStatement();
+            
+            ResultSet resultado = comando.executeQuery(sql);
+            // Cria uma lista de produtos vazia
+            List<Usuario> usuarios = new LinkedList<>();
+            while (resultado.next()) {
+                // Inicializa um objeto de produto vazio
+                Usuario tmp = new Usuario();
+                // Pega os valores do retorno da consulta e coloca no objeto
+                tmp.setId(resultado.getInt("Id"));
+                tmp.setNome(resultado.getString("Nome"));
+                
+                // Pega o objeto e coloca na lista
+                usuarios.add(tmp);
+            }
+            return usuarios;
+        } catch (SQLException ex) {
+            Logger.getLogger(DAOUsuario.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
 }
